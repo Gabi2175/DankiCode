acbcaf4813f65fec6ab6ff093ada25e11b5ec369
Gabi2175 <gabrieldalbem2175@gmail.com>
2024-10-31 16:21:50 -0300

    variaveis e dados

--- Python/Aula01.py
+++ Python/Aula01.py
@@ -1,1 +1,17 @@
 print ("ola mundo testando atalho"); 
+ana = "curso de pyton"
+print (ana)
+
+#testando metodo das strings
+
+b = " ola mundo "
+print(b.strip()) #strip() ignora os espacos inical e final;
+
+c = "OLA MUNDO"
+print(c.lower()) # lower() converte maiusculo em minusculo uper() faz o oposto
+
+
+
+
+
+
